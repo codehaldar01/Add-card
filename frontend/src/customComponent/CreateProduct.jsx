a5f627f3ae50ad11cace796796a17ea7e0b2e135
codehaldar01 <96612102+codehaldar01@users.noreply.github.com>
2024-12-24 09:23:04 +0530

    Update CreateProduct.jsx
    updated api url to connect to the backend

--- frontend/src/customComponent/CreateProduct.jsx
+++ frontend/src/customComponent/CreateProduct.jsx
@@ -10,7 +10,7 @@ const CreateProduct = () => {
   const handleSubmit = async(e)=>{
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/products",{
+    const response = await fetch("https://add-card-7epj.onrender.com/api/products",{
       method: "POST",
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newProduct)
